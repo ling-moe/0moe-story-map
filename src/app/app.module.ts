71cb02bacd68519fbef97b42c8b231e2126592fb
yukdawn <yukdawn@gmail.com>
2022-08-22 08:50:21 +0800

    add: report

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { FeatureCardComponent } from './feature-card/feature-card.component';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatMenuModule } from '@angular/material/menu';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { InlineEditComponent } from './inline-edit/inline-edit.component';
 import { FeatureAreaComponent } from './feature-area/feature-area.component';
-import {MatTableModule} from '@angular/material/table';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 
@@ -33,15 +29,11 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     FormsModule,
     BrowserAnimationsModule,
     DragDropModule,
-    MatCardModule,
     MatIconModule,
     MatButtonModule,
-    MatGridListModule,
     MatInputModule,
     MatFormFieldModule,
-    MatMenuModule,
     MatSelectModule,
-    MatTableModule,
     FlexLayoutModule,
   ],
   providers: [],
